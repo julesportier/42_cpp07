0745eee8c81d8d2fe44a1236a9ff7d9fd9f698b3
Jules Portier <142106821+julesportier@users.noreply.github.com>
2026-01-07 12:58:07 +0100

    feat(ex02): output stream operator for Array class

--- ex02/src/Array.h
+++ ex02/src/Array.h
@@ -68,14 +68,28 @@ void fill_array(Array<T>& a, unsigned int start, unsigned int step)
 }
 
 template <typename T>
+std::ostream& operator<<(std::ostream& os, Array<T>& array)
+{
+	unsigned int size = array.size();
+	if (size == 0) {
+		os << "[]";
+		return (os);
+	}
+	os << "[";
+	for (unsigned int i = 0; i < size - 1; ++i) {
+		os << array[i] << ", ";
+	}
+	os << array[size - 1] << "]\n";
+	return (os);
+}
+
+template <typename T>
 void print_array(Array<T>& a, const std::string& name)
 {
 	std::cout << "# " << name << " #"<< '\n';
 	std::cout << "object address: " << &a << '\n';
 	std::cout << "member array address: " << &(a[0]) << '\n';
-	for (unsigned int i = 0; i < a.size(); ++i) {
-		std::cout << a[i] << '\n';
-	}
+	std::cout << a << '\n';
 }
 
 #endif
